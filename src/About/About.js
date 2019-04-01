068a1e78eb7040caab048749213ed64fcaf263c9
jenniferyuexiao <46546080+jenniferyuexiao@users.noreply.github.com>
2019-04-01 16:57:46 -0700

    Update About.js

--- src/About/About.js
+++ src/About/About.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import style from './About.module.css';
 
 const About = () => (
+<div>
+  <div className = {style.about}>
   <h1>About</h1>
+  <strong><p>About us</p></strong>
+  </div>
+</div>
 );
 
 export default About;
