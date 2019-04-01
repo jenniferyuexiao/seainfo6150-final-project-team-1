a34b3ccc1166d61623ca3c57b3d9364e7b18347e
jenniferyuexiao <46546080+jenniferyuexiao@users.noreply.github.com>
2019-04-01 16:56:38 -0700

    Update Contact.js

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import style from './Contact.module.css';
 
 const Contact = () => (
-  <h1>Contact</h1>
-);
+<div>
+<div className = {style.contact}>
+    <h1>Contact</h1>
+    <p>Feel free to contact us</p>
+    <p>Address: 401 Terry Street, Seattle, WA 98109</p>
+</div>
+</div>
+
+)
+
+
 
 export default Contact;
