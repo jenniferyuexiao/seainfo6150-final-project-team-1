9e0f465e6d827d6e97eae38546796e9a10b3719b
jenniferyuexiao <46546080+jenniferyuexiao@users.noreply.github.com>
2019-04-01 16:59:32 -0700

    Add files via upload

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import style from './NotFound.module.css';
 
 const NotFound = () => (
+<div>
+  <div className = {style.notfound}>
   <h1>404</h1>
-);
+  <p>404.That's an error.</p>
+  <p>The request of URL was found on this sever.</p>
+  </div>
+</div>
+)
 
 export default NotFound;
